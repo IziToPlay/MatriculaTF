74dde34f01d5b3404dbdabf26c00073d4ca83231
JosLui <joslui1720082@gmail.com>
2020-05-17 13:23:27 -0500

    first commit

---
+++ src/main/java/com/matricula/MatriculaTfApplication.java
@@ -0,0 +1,13 @@
+package com.matricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaTfApplication.class, args);
+	}
+
+}
