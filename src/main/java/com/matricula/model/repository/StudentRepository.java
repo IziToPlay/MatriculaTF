51eec8d602b566f5c680434cc62fddc60d3be924
LuisDextre99 <luisdexsmr@gmail.com>
2020-05-17 16:19:59 -0500

    Update

--- src/main/java/com/matricula/model/repository/StudentRepository.java
+++ src/main/java/com/matricula/model/repository/StudentRepository.java
@@ -2,10 +2,9 @@ package com.matricula.model.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +12,7 @@ import com.matricula.model.entity.Student;
 
 
 @Repository
-public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 	
 	/**
      * @return newest articleId
@@ -27,8 +26,8 @@ public interface StudentRepository extends PagingAndSortingRepository<Student, L
      * @return          a page of entities that fulfill the restrictions
      *                  specified by the Pageable object
      */
-	Page<Student> findAll(Pageable pageable);
+	List<Student> findAll();
 
 	@Query("SELECT s FROM Student s WHERE s.id like %?1%")
-	Page<Student> finById(String id,Pageable pageable);
+	List<Student> finById(String id,Pageable pageable);
 }
