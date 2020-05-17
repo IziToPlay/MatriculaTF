0f7138d8c6a6938248732e678f980c6ce0dbbcb4
Miguel Calderon <miguel15081998@protonmail.com>
2020-05-17 18:15:54 -0500

    aa

--- src/main/java/com/matricula/model/repository/StudentCourseRepository.java
+++ src/main/java/com/matricula/model/repository/StudentCourseRepository.java
@@ -10,13 +10,7 @@ import com.matricula.model.entity.StudentCourse;
 
 @Repository
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
-	/*
-	@Query("select t from Trip t where t.dptDeparture.name like %?1% and t.dptArrival.name like %?2% and t.startDate like %?3%")
-	List<Trip> fetchTripByDpt(String dptDeparture,String dptArrival, String startDate);
 	
-	@Query("select t from Trip t where t.dptArrival like %?1%")
-	List<Trip> fetchTripByDptArrival(String dptArrival);
-	*/
 	
 	@Query("select sc from StudentCourse sc where sc.enrollment.semester like %?1%")
 	List<StudentCourse> fetchStudentCourseBySemester(Integer semester);
