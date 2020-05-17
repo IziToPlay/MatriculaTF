41db4bc503df356a3a485758661dc9d8c3361a21
Miguel Calderon <miguel15081998@protonmail.com>
2020-05-17 17:53:05 -0500

    Implementacion de EnrollmentRepository

--- src/main/java/com/matricula/model/repository/EnrollmentRepository.java
+++ src/main/java/com/matricula/model/repository/EnrollmentRepository.java
@@ -3,10 +3,10 @@ package com.matricula.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.matricula.model.entity.Account;
+import com.matricula.model.entity.Enrollment;
 
 @Repository
-public interface UserRepository extends JpaRepository<Account, Long>{
-	Account findByUserName(String userName);
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+	
 	
 }
