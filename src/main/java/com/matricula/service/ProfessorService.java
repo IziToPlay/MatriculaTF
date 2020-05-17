e55f6d127b7443401cfeeeaa8446b3c7c32607ee
marcosrm7 <m2066r@gmail.com>
2020-05-17 16:25:52 -0500

    professor service

--- src/main/java/com/matricula/service/ProfessorService.java
+++ src/main/java/com/matricula/service/ProfessorService.java
@@ -1,5 +1,10 @@
 package com.matricula.service;
 
+import java.util.List;
+import com.matricula.model.entity.Professor;
+
 public interface ProfessorService {
-oli
+	public void insert(Professor i);
+	public List<Professor> list();
+	
 }
