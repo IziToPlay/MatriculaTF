ad5eaa006ef8e46d7baa405953cfc7b68ea4a2d5
Manuel <m2066r@gmail.com>
2020-05-17 15:47:44 -0500

    entity professor

--- src/main/java/com/matricula/model/entity/Professor.java
+++ src/main/java/com/matricula/model/entity/Professor.java
@@ -1,5 +1,56 @@
 package com.matricula.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity 
+@Table(name="professors")
+
 public class Professor {
-/*aAaaaaaaaaaaaaaaaaaaaaaaaaaaa*/
+
+	@Id
+	@Column(name = "professor_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	
+	@Column(name = "name")
+	@NotEmpty(message="Ingrese nombre")
+	private String name;
+	
+	@Column(name = "lastName")
+	@NotEmpty(message="Ingrese apellido")
+	private String lastName;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+
 }
