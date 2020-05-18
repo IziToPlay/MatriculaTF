ec1e245f202ff8108d894fbcb39ea6b2282f9c13
JosLui <joslui1720082@gmail.com>
2020-05-17 21:21:27 -0500

    Metodos implementados en UserRepositoryUserService e Impl

---
+++ src/main/java/com/matricula/service/UserService.java
@@ -0,0 +1,11 @@
+package com.matricula.service;
+
+import com.matricula.model.entity.Account;
+
+public interface UserService {
+	
+	Account findById(Long id);
+	
+	Account findByUserName(String userName);
+
+}
