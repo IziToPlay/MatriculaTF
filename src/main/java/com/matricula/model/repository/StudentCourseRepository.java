f354792f455f7f89032356966b9520fd08c12adf
JosLui <joslui1720082@gmail.com>
2020-05-17 20:21:37 -0500

    ef

--- src/main/java/com/matricula/model/repository/StudentCourseRepository.java
+++ src/main/java/com/matricula/model/repository/StudentCourseRepository.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.matricula.model.entity.Student;
 import com.matricula.model.entity.StudentCourse;
 
 @Repository
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
 	
 	
-	@Query("select sc from StudentCourse sc where sc.enrollment.semester like %?1%")
-	List<StudentCourse> fetchStudentCourseBySemester(Integer semester);
+	@Query("select sc from StudentCourse sc where sc.enrollment.semester=?1 AND sc.student.id=student.id")
+	List<StudentCourse> fetchStudentCourseBySemester(Integer semester,@Param ("student")Student student);
 	
+		
 }
