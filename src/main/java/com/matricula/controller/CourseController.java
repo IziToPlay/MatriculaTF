904c8717647964812852490590e394368d65f6e0
JosLui <joslui1720082@gmail.com>
2020-06-17 18:10:50 -0500

    dwed

--- src/main/java/com/matricula/controller/CourseController.java
+++ src/main/java/com/matricula/controller/CourseController.java
@@ -105,7 +105,7 @@ public class CourseController {
 		courseService.updateCourse(id, course);
         model.addAttribute("success", "Curso actualizado correctamente");
 		model.addAttribute("courses", courseService.getAllCourses());
-		return "/courses/list";
+		return "courses/list";
 		} else {
 			model.addAttribute("error","Debe completar todos los campos");
 			model.addAttribute("course", courseToEdit);
